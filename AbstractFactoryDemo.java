d02e6562c5ff360e9855bbba384b53fd3a852c73
Rick Kock <rick.92.tlf@gmail.com>
2017-07-01 13:39:46 +0200

    Initial commit.

---
+++ AbstractFactoryDemo.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package abstract_factory;
+
+/**
+ * @author Rick
+ *
+ */
+public class AbstractFactoryDemo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
